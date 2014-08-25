8c301063b6fd9d003a3a362f6ece53df3c586536
mio <mio.shibuya@gmail.com>
2014-08-25 12:00:20 +0900

    relationにorderを指定

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,5 +1,5 @@
 class Community < ActiveRecord::Base
   has_many :joinings
-  has_many :users, through: :joinings
-  has_many :rooms
+  has_many :users, -> { order 'joinings.created_at DESC'}, through: :joinings
+  has_many :rooms, -> { order(created_at: :desc) }
 end
