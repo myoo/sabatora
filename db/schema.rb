746d38369b7d121bb5fdda529dd68dab2db69c17
arupaka <t.tanaka@trictrac.co.jp>
2014-09-05 17:28:27 +0900

    セッションログのDL関連処理

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826070054) do
+ActiveRecord::Schema.define(version: 20140905023132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20140826070054) do
     t.integer  "system_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "paramaters"
+    t.integer  "retry_number"
+    t.text     "profile"
+    t.text     "memo"
+    t.text     "status"
+    t.string   "image"
   end
 
   add_index "characters", ["user_id"], name: "index_characters_on_user_id", using: :btree
