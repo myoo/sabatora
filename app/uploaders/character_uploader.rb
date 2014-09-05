ee747289c6f627bffd9bb6a70690257a8b7c5133
arupaka <t.tanaka@trictrac.co.jp>
2014-09-05 12:03:06 +0900

    キャラシートのデフォルト画像設定

--- app/uploaders/character_uploader.rb
+++ app/uploaders/character_uploader.rb
@@ -14,7 +14,7 @@ class CharacterUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default_character.png"].compact.join('_'))
 
     # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
