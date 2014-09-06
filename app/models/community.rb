8f733653487b9ff71bcb540ae9b06b39bf9c9ce3
umanoda <hhenpitu@gmail.com>
2014-09-06 23:27:11 +0900

    current_user=nilの時に、communitys/indexが表示できないのを修正

--- app/models/community.rb
+++ app/models/community.rb
@@ -5,7 +5,7 @@ class Community < ActiveRecord::Base
   has_many :backgrounds
 
   def has_member?(user)
-    self.joinings.where(user_id: user.id).length > 0
+    user.present? and self.joinings.where(user_id: user.id).length > 0
   end
 
   def is_owner?(user)
