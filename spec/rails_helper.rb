53e31b271241a3cab746e560ea8035df324a6450
mio <mio.shibuya@gmail.com>
2014-09-22 14:29:46 +0900

    turnip導入・設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,9 +13,20 @@ require 'rspec/rails'
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
+  Dir.glob("spec/**/*steps.rb") { |f| load f, true }
+
+  require 'capybara/dsl'
+  require 'capybara/rspec'
+  require 'capybara/webkit'
+  require 'capybara/poltergeist'
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  require 'turnip'
+  require 'turnip/capybara'
+
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+# ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -24,20 +35,12 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
+
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
+
+
   config.infer_spec_type_from_file_location!
 end
