9432d5fe572b78d41d029de1da2e9795f4c76407
mio <mio.shibuya@gmail.com>
2014-09-25 16:08:13 +0900

    シナリオ「プロフィール登録」をクリア

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 class ProfilesController < ApplicationController
   before_filter :authenticate_user!, except: [:show]
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
+  before_action :already_profiled, only: [:new]
 
   # GET /profiles/1
   # GET /profiles/1.json
@@ -22,6 +24,15 @@ class ProfilesController < ApplicationController
   def create
     @profile = Profile.new(profile_params)
     @profile.user = current_user
+    # プレビュー画面を挿入
+    if params[:preview]
+      unless @profile.valid?
+        render :new
+      else
+        render :preview
+      end
+      return
+    end
 
     respond_to do |format|
       if @profile.save
@@ -37,6 +48,16 @@ class ProfilesController < ApplicationController
   # PATCH/PUT /profiles/1
   # PATCH/PUT /profiles/1.json
   def update
+    # プレビュー画面を挿入
+    if params[:preview]
+      unless @profile.valid?
+        render :edit
+      else
+        render :preview
+      end
+      return
+    end
+
     respond_to do |format|
       if @profile.update(profile_params)
         format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
@@ -66,6 +87,13 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:user_id, :introduction, :sex, :avatar, :birth)
+      params.require(:profile).permit(:user_id, :introduction, :sex, :avatar, :birth, :remove_avatar, :avatar_cache)
+    end
+
+    def already_profiled
+      if current_user.profile.present?
+       flash[:alert] = "既にプロフィールが登録されています"
+        redirect_to root_path
+      end
     end
 end
--- spec/steps/user_profile_steps.rb
+++ spec/steps/user_profile_steps.rb
@@ -23,6 +23,43 @@ module UserProfileSteps
   step "プロフィール設定画面が表示される" do
     expect(page).to have_content("プロフィールを登録してください")
   end
+
+  step "プロフィールを入力する" do
+    within "#new_profile" do
+      attach_file "profile[avatar]", File.expand_path("spec/support/testfiles/flower.jpg")
+      choose "female"
+      select "1984", from: 'profile[birth(1i)]'
+      select "1月", from: 'profile[birth(2i)]'
+      select "1", from: 'profile[birth(3i)]'
+      fill_in "profile[introduction]", with: "テストのプロフィール"
+    end
+  end
+
+  step "登録ボタンを押す" do
+    click_button "登録する"
+  end
+
+  step "確認画面に内容が表示される" do
+    expect(page).to have_xpath("//img[contains(@src, \"flower.jpg\")]")
+    expect(page).to have_content("female")
+    expect(page).to have_content("1984-01-01")
+    expect(page).to have_content("テストのプロフィール")
+  end
+
+  step "実行ボタンを押す" do
+    click_button "実行する"
+  end
+
+  step "プロフィール画面が表示される" do
+    expect(page).to have_content("#{@name}さんのプロフィール")
+  end
+
+  step "変更内容が反映されている" do
+    expect(page).to have_xpath("//img[contains(@src, \"flower.jpg\")]")
+    expect(page).to have_content("female")
+    expect(page).to have_content("1984-01-01")
+    expect(page).to have_content("テストのプロフィール")
+  end
 end
 
 require "steps/member_registration_steps.rb"
