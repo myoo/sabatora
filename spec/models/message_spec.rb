16e0b6485d29b3e4468a2ab64f380797f40abf49
mio <mio.shibuya@gmail.com>
2014-10-06 19:14:41 +0900

    リロード時に表示されるログが過去５００件になっていたのを修正

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,16 @@
+# -*- coding: utf-8 -*-
 require 'rails_helper'
 
 RSpec.describe Message, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#resent_logs" do
+    before do
+      FactoryGirl.create_list(:message, 510)
+    end
+
+    it "最新500件が表示されること" do
+      expect(Array(Message.recent_logs(1)).count).to be 500
+      expect(Message.recent_logs(1).last.body).to match(/510|509/)
+#      expect(Message.recent_logs(1).first.body).to include(/10|9/)
+    end
+  end
 end
