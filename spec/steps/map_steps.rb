15b4166e8c9e5f8ddcd91181e10c509981ef5f30
mio <mio.shibuya@gmail.com>
2014-10-14 16:21:03 +0900

    マップ作成まで

--- spec/steps/map_steps.rb
+++ spec/steps/map_steps.rb
@@ -6,6 +6,31 @@ module MapSteps
     visit new_community_map_path(@community)
     expect(page).to have_css('h1', "マップの新規登録")
   end
+
+  step "タイトルに :mapname を入力する" do |mapname|
+    fill_in "タイトル", with: mapname
+  end
+
+  step "説明を入力する" do
+    fill_in "説明", with: "説明文"
+  end
+
+  step "画像を登録する" do
+    attach_file '画像', "#{Rails.root}/spec/support/testfiles/map.jpg"
+  end
+
+  step "登録ボタンをクリックする" do
+    click_button "登録する"
+  end
+
+  step "マップが作成される" do
+    expect(page).to have_content("マップを作成しました")
+  end
+
+  step "マップの一覧に :mapname が表示される" do |mapname|
+    visit community_maps_url(@community)
+    expect(page).to have_content(mapname)
+  end
 end
 
 RSpec.configure { |c| c.include MapSteps }
